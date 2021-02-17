9b49fa4af791a72f6a50db29070046833be61706
Alex Bie <alexbie98@gmail.com>
2021-02-16 19:03:46 -0500

    shell call to test script

--- src/test/Test.cc
+++ src/test/Test.cc
@@ -7,11 +7,13 @@ const string CHECKMARK = "\u2714";
 const string XMARK = "\u2718";
 
 bool runIOTest(const string& filePath, const string& name, int expect){
-    cout << "| " << name;
 
-    int result = 0;
+    string command = "./joosc " + filePath;
+    int result = system(command.c_str());
+
     bool passed = result == expect;
 
+    cout << "| " << name;
     if (passed){
         cout << " " << CHECKMARK << endl;
     }
